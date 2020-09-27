eb334a4aa77d6fbc45b9e5b8fa92090a7288f4d0
David Tverdota <fireblade.sg@gmail.com>
2020-09-27 17:58:11 +0100

    Fixing balance displayed as zero

--- repository/src/main/java/com/fireblade/repository/network/domain/NetworkWallet.kt
+++ repository/src/main/java/com/fireblade/repository/network/domain/NetworkWallet.kt
@@ -1,5 +1,8 @@
 package com.fireblade.repository.network.domain
 
+import com.google.gson.annotations.SerializedName
+
 data class NetworkWallet(
+    @SerializedName("final_balance")
     val finalBalance: Long
 )
